44e21c986aff6b984467bdbd931c33696e6ba2ea
Divya <divyajunghare0403@gmail.com>
2023-06-06 11:09:42 +0530

    --

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -12,9 +12,9 @@ export interface Row {
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: serverResponds[]):Row {
-    const priceABC=(serverResponds[0].top_ask.price+serverResponds[0].top_bid.price)/2;
-    const priceDEF=(serverResponds[1].top_ask.price+serverResponds[1].top_bid.price)/2;
+  static generateRow(serverRespond: ServerRespond[]):Row {
+    const priceABC=(serverRespond[0].top_ask.price+serverRespond[0].top_bid.price)/2;
+    const priceDEF=(serverRespond[1].top_ask.price+serverRespond[1].top_bid.price)/2;
     const ratio=priceABC/priceDEF;
     const upperBound=1+0.05;
     const lowerBound=1-0.05;
@@ -22,8 +22,8 @@ export class DataManipulator {
         price_abc:priceABC,
         price_def:priceDEF,
         ratio,
-        timestamp: serverResponds[0].timestamp>serverResponds[1].timestamp ?
-          serverResponds[0].timestamp : serverResponds[1].timestamp,
+        timestamp: serverRespond[0].timestamp>serverRespond[1].timestamp ?
+          serverRespond[0].timestamp : serverRespond[1].timestamp,
         upper_bound:upperBound,
         lower_bound:lowerBound,
         trigger_alert:(ratio>upperBound || ratio<lowerBound) ? ratio:undefined,
