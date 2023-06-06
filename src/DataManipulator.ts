a7c9d969a102dc5829375589bae214bcc005d800
Divya <divyajunghare0403@gmail.com>
2023-06-06 11:05:01 +0530

    -

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -12,8 +12,8 @@ export interface Row {
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: serverRespond[]):Row {
-    const priceABC=(serverRespond[0].top_ask.price+serverRespond[0].top_bid.price)/2;
+  static generateRow(serverResponds: serverResponds[]):Row {
+    const priceABC=(serverResponds[0].top_ask.price+serverResponds[0].top_bid.price)/2;
     const priceDEF=(serverResponds[1].top_ask.price+serverResponds[1].top_bid.price)/2;
     const ratio=priceABC/priceDEF;
     const upperBound=1+0.05;
